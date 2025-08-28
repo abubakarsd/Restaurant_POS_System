c1957a2bdb7fe83df658d9d095f61ec4304fb58e
Abubakar Haruna Abdulmalik <42899173+abubakarsd@users.noreply.github.com>
2025-08-28 16:47:29 +0100

    update API

--- pos-backend/config/config.js
+++ pos-backend/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = Object.freeze({
     port: process.env.PORT || 3000,
-    databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/pos-db",
+    databaseURI: process.env.MONGODB_URI || "mongodb+srv://sdharuna:XO7iGAFvkEuraV4E@pos-cluster.bsfag5o.mongodb.net/?retryWrites=true&w=majority&appName=pos-cluster",
     nodeEnv : process.env.NODE_ENV || "development",
     accessTokenSecret: process.env.JWT_SECRET,
     razorpayKeyId: process.env.RAZORPAY_KEY_ID,
